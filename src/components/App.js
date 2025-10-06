2022639b78d764cf4d230fe71eae0164861a28a2
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-07 00:26:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
  const App=()=>{
    return(
      <div className="app">
-     
+        <h1 className="title">Markdown Editor</h1>
+        // <MarkdownEditor />
      </div>
    )
  };
